07357f0ad48b4b0e31321ffe17fc73bee331e9f4
Kanan <43238830+KenanBoyukkishiyev1997@users.noreply.github.com>
2018-09-28 17:46:33 +0400

    C# CONVERSION

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("eded daxil edin");
+            int s;
+            bool parsed = int.TryParse(Console.ReadLine(), out s);
+            if (parsed)
+            {
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine("daxil etdiyiniz eded budu :" + s + '*' + i + "=" + s * i);
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("warrning " );
+            }
+          
+               
+            
+
+        }
+    }
+}
